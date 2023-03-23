545fdef85e9ad58ed6264dee80e7e2ae5bafa53c
thinhdoo <dohungthinh247@gmail.com>
2023-03-24 00:13:09 +0700

    ss
    xxx

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import animateTitle from './features/animateTitle'
 import createBadge from './features/createBasge'
 import './styles/style.css'
 
-console.log('anH YEdsssU EM')
+console.log('anH YEdsxxxssU EM')
 
 createBadge()
 animateTitle()
